9bb8a55797ccbbf1db54a48d1afd39970a04fd94
uzumaxy <uzumaxy@gmail.com>
2015-08-01 03:19:46 +0300

    Added sources.

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'jss:jquery-shutter',
+  version: '0.0.1',
+  summary: 'Shutter Effect Portfolio with jQuery and Canvas',
+  git: 'https://github.com/JSSolutions/meteor-jquery-shutter'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use('jquery');
+  api.addFiles(['jquery-shutter.js', 'jquery-shutter.css'], 'client');
+});
