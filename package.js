a468ade3d1700a1be92751dbc855e77346fa569e
VolodymyrTymets <volodymyrtymets@gmail.com>
2016-02-05 10:56:18 +0200

    Update package.js
    update package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jss:jquery-shutter',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Shutter Effect Portfolio with jQuery and Canvas',
   git: 'https://github.com/JSSolutions/meteor-jquery-shutter'
 });
